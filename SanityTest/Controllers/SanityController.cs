478652279692fe9a95600b0c559c881a29c9150f
Mel Grubb <melgrubb@live.com>
2015-02-19 08:05:53 -0500

    Initial commit

---
+++ SanityTest/Controllers/SanityController.cs
@@ -0,0 +1,34 @@
+﻿using SanityTest.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SanityTest.Controllers
+{
+    public class SanityController : Controller
+    {
+        // GET: Sanity
+        public ViewResult Index()
+        {
+            var choices = new List<SelectListItem>
+            {
+                new SelectListItem {Text = "Select", Value = ""},
+                new SelectListItem {Text = "Value 1", Value = "1"},
+                new SelectListItem {Text = "Value 2", Value = "2"},
+                new SelectListItem {Text = "Value 3", Value = "3"},
+                new SelectListItem {Text = "Value 4", Value = "4"}
+            };
+
+            var model = new SanityIndexViewModel
+            {
+                Choice1 = null,
+                Choice2 = "1",
+                Choice3 = null,
+                Choice4 = "2",
+                Choice5 = null,
+                Choices = choices
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ SanityTest/Models/SanityIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SanityTest.Models
+{
+    public class SanityIndexViewModel
+    {
+        [Display(Name = "Choice #1")]
+        public string Choice1 { get; set; }
+
+        [Display(Name = "Choice #2")]
+        public string Choice2 { get; set; }
+
+        [Display(Name = "Choice #3")]
+        public string Choice3 { get; set; }
+
+        [Display(Name = "Choice #4")]
+        public string Choice4 { get; set; }
+
+        [Display(Name = "Choice #5")]
+        public string Choice5 { get; set; }
+
+        public IEnumerable<SelectListItem> Choices { get; set; }
+    }
+}
